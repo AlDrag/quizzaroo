e0f0d885b00eb9db00f04dcd0287ce4e05d2335d
Ryan Lewis <ryansname@gmail.com>
2024-01-23 18:51:33 +1300

    Easier to manage multiline commands

--- open.sh
+++ open.sh
@@ -6,9 +6,10 @@
 # - JQ
 # - internet
 
-curl 'https://www.stuff.co.nz/_json/national/quizzes?limit=10' |\
-    jq '.stories[] | {title: .title, url: .html_assets[0].data_content | capture("iframe.*src=\"(?<a>[^?]*)").a}' |\
-    jq -r '"<a href=\"\(.url)\">\(.title)</a><br>"' > /tmp/quizaroo.html
+curl 'https://www.stuff.co.nz/_json/national/quizzes?limit=10' \
+    | jq '.stories[] | {title: .title, url: .html_assets[0].data_content | capture("iframe.*src=\"(?<a>[^?]*)").a}' \
+    | jq -r '"<a href=\"\(.url)\">\(.title)</a><br>"' \
+    > /tmp/quizaroo.html
 
 CMD=$(which xdg-open || which open)
 $CMD /tmp/quizaroo.html
