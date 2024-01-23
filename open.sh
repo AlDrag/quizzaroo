dbed41a7113f7224bd31783672fdcc2e85a060d8
Ryan Lewis <ryansname@gmail.com>
2024-01-23 18:48:54 +1300

    Tweak the url opener detection logic

--- open.sh
+++ open.sh
@@ -13,5 +13,5 @@ curl 'https://www.stuff.co.nz/_json/national/quizzes?limit=10' |\
     jq '.stories[] | {title: .title, url: .html_assets[0].data_content | capture("iframe.*src=\"(?<a>[^?]*)").a}' |\
     jq -r '"<a href=\"\(.url)\">\(.title)</a><br>"' > /tmp/quizaroo.html
 
-CMD=$(which open > /dev/null && echo "open" || echo "xdg-open")
+CMD=$(which xdg-open || which open)
 $CMD /tmp/quizaroo.html
